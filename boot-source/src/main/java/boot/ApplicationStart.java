9a652859304c1f9c11bfe26efeb49ae36a9414e9
PengFuLin <1162855925@qq.com>
2022-03-13 22:19:31 +0800

    V1.1.0：  -完成springboot的核心使用理论  -构建java-log模块&整合springboot

---
+++ boot-source/src/main/java/boot/ApplicationStart.java
@@ -0,0 +1,14 @@
+package boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author PengFuLin
+ * @version 1.0
+ * @description: 启动类
+ * @date 2022/3/7 16:43
+ */
+@SpringBootApplication
+public class ApplicationStart {
+
+}
