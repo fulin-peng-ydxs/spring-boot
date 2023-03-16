b2806c7901474302d766467a6beda88592e5d517
PengFuLin <1162855925@qq.com>
2023-03-17 00:07:59 +0800

    V2.4.4  -加入mybatis-plus集成  -完善spring-boot基础理论

---
+++ boot-data/data-mybatis-plus/src/main/java/plus/Application.java
@@ -0,0 +1,15 @@
+package plus;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("plus.dao.basic")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
---
+++ boot-data/data-mybatis-plus/src/main/java/plus/entity/Organ.java
@@ -0,0 +1,9 @@
+package plus.entity;
+
+import lombok.Data;
+
+@Data
+public class Organ {
+    private Long id;
+    private String orgName;
+}
