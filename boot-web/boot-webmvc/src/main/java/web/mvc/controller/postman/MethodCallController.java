b300efc013df614000381eeef2cd39ede49445fb
pengfulin <1162855925@qq.com>
2023-12-28 00:11:11 +0800

    加入服务方法调用控制器

---
+++ boot-web/boot-webmvc/src/main/java/web/mvc/controller/postman/MethodCallController.java
@@ -0,0 +1,120 @@
+package web.mvc.controller.postman;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.*;
+import web.mvc.model.web.response.Response;
+import web.mvc.util.JsonUtils;
+import web.mvc.util.ProxyUtils;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 服务方法调用-控制器
+ *
+ * @author pengshuaifeng
+ * 2023/12/27
+ */
+@Api(tags = "后台控制-服务方法调用")
+@Slf4j
+@RequestMapping("/postman/service/method")
+@RestController
+public class MethodCallController {
+
+    @Autowired
+    private ApplicationContext context;
+
+
+    @ApiOperation(value="方法调用",produces = "application/json",
+            notes ="serviceName：为所要调用服务名称（必填）\n"
+                    +"serviceMethod：为所要调用的服务的具体方法（必填）\n"
+                    +"methodParam：为方法的参数值（数组格式，按照前后顺序匹配，没有参数则不用填！如果参数是对象，请用json方式传入)\n"
+                    +"methodParamClass：为方法的参数类型（数组格式，按照前后顺序匹配，没有参数则不用填！例如：[String.class]、[String.class,Integer.class,{}]）"
+    )
+    @RequestMapping(value = { "/call/{proxyType}" }, method = { RequestMethod.POST })
+    public Response methodCall(@PathVariable String proxyType, @RequestBody Map<String,Object> paramMap){
+        log.debug("进入-服务方法调用控制器：{}/{}",proxyType,paramMap);
+        try {
+            String serviceName =paramMap.get("serviceName").toString();
+            String serviceMethodName = paramMap.get("serviceMethod").toString();
+            //获取服务对象
+            Object serviceObjet = context.getBean(serviceName);
+            if(proxyType.equals("basic")){   // 不用代理对象
+                serviceObjet= ProxyUtils.getTarget(serviceObjet);
+            }
+            Class<?> serviceObjectClass = serviceObjet.getClass();
+            //获取服务方法
+            Method serviceMethod=null;
+            List<String> methodParams = (List<String>)paramMap.get("methodParam");
+            Class<?>[] methodParamClassesTypes=null;
+            boolean notParamMethod = methodParams == null || methodParams.isEmpty();
+            if(notParamMethod){
+                serviceMethod=serviceObjectClass.getDeclaredMethod(serviceMethodName);
+            }else{
+                List<String> methodParamClasses=(List<String>)paramMap.get("methodParamClass");
+                methodParamClassesTypes=new Class<?>[methodParamClasses.size()];
+                for (int i = 0; i < methodParamClasses.size(); i++) {
+                    methodParamClassesTypes[i]=Class.forName(methodParamClasses.get(i));
+                }
+                serviceMethod=serviceObjectClass.getDeclaredMethod(serviceMethodName,methodParamClassesTypes);
+            }
+            //调用服务方法
+            serviceMethod.setAccessible(true);
+            Object result=null;
+            if(notParamMethod){
+                result=serviceMethod.invoke(serviceObjet);
+            }else{
+                Object[] methodParamValues=new Object[methodParams.size()];
+                for (int i = 0; i < methodParams.size(); i++) {
+                    String methodParam = methodParams.get(i);
+                    Class<?> methodParamClassesType = methodParamClassesTypes[i];
+                    //TODO 仅支持引用数据类型，暂不支持基本数据类型
+                    Object methodParamValue;
+                    if (methodParamClassesType==String.class) {
+                        methodParamValue= methodParam;
+                    }else if(methodParamClassesType==Integer.class){
+                        methodParamValue= Integer.valueOf(methodParam);
+                    }else if(methodParamClassesType==Boolean.class){
+                        methodParamValue= Boolean.valueOf(methodParam);
+                    }else{
+                        if (methodParamClassesType.isEnum()) {
+                            methodParamValue= Enum.valueOf((Class<? extends Enum>) methodParamClassesType,methodParam);
+                        }else{
+                            //TODO 暂不支持Collection集合参数
+                            methodParamValue= JsonUtils.getObject(methodParam,methodParamClassesType);
+                        }
+                    }
+                    methodParamValues[i]=methodParamValue;
+                }
+                result=serviceMethod.invoke(serviceObjet,methodParamValues);
+            }
+            log.debug("退出-服务方法调用控制器：{}",result);
+            return Response.success(result);
+        }catch (InvocationTargetException e) {   //服务方法内部异常
+            Throwable trowEx = e.getTargetException();
+            log.error("服务方法内部异常：",trowEx);
+            return exception(e,"服务方法内部异常");
+        }catch (Exception e){ //服务方法调用异常
+            log.error("服务方法调用异常：",e);
+            return exception(e,"服务方法调用异常");
+        }
+    }
+
+    /**
+     * 异常统一处理
+     * 2023/12/27 23:48
+     * @author pengshuaifeng
+     */
+    private Response exception(Exception e,String defaultErrorMsg) {
+        String errorMeg = defaultErrorMsg;
+        if (e instanceof RuntimeException)
+            errorMeg = e.getMessage();
+        return Response.failure(errorMeg);
+    }
+}
---
+++ boot-web/boot-webmvc/src/main/java/web/mvc/util/ProxyUtils.java
@@ -0,0 +1,65 @@
+package web.mvc.util;
+
+import org.springframework.aop.framework.AdvisedSupport;
+import org.springframework.aop.framework.AopProxy;
+import org.springframework.aop.support.AopUtils;
+
+import java.lang.reflect.Field;
+
+/**
+ * 代理工具类
+ * @author Peng fu lin
+ * 2021-12-22 11:30
+ */
+public class ProxyUtils {
+
+    /**获取代理对象
+     * 2023/8/6-0:30
+     * @author pengfulin
+     * @return 返回e对象的代理对象，如果e不是代理对象，则返回e本身
+    */
+    public static  Object getTarget(Object e) throws Exception {
+        if( !isProxy(e)){ //不是代理对象，则返回原对象
+            return e;
+        }
+        if(AopUtils.isJdkDynamicProxy(e)) {
+            return getJdkDynamicProxyTargetObject(e);
+        } else { //cglib
+            return getCglibProxyTargetObject(e);
+        }
+    }
+
+
+    /**判断proxy对象是否为代理对象
+     * 2023/8/6-0:31
+     * @author pengfulin
+    */
+    public static  boolean isProxy(Object proxy){
+        return AopUtils.isAopProxy(proxy);
+    }
+
+
+    private static Object getCglibProxyTargetObject(Object proxy) throws Exception {
+        Field h = proxy.getClass().getDeclaredField("CGLIB$CALLBACK_0");
+        h.setAccessible(true);
+        Object dynamicAdvisedInterceptor = h.get(proxy);
+
+        Field advised = dynamicAdvisedInterceptor.getClass().getDeclaredField("advised");
+        advised.setAccessible(true);
+
+        return ((AdvisedSupport)advised.get(dynamicAdvisedInterceptor)).getTargetSource().getTarget();
+    }
+
+    private static Object getJdkDynamicProxyTargetObject(Object proxy) throws Exception {
+        Field h = proxy.getClass().getSuperclass().getDeclaredField("h");
+        h.setAccessible(true);
+        AopProxy aopProxy = (AopProxy) h.get(proxy);
+
+        Field advised = aopProxy.getClass().getDeclaredField("advised");
+        advised.setAccessible(true);
+
+        return ((AdvisedSupport)advised.get(aopProxy)).getTargetSource().getTarget();
+    }
+
+
+}
