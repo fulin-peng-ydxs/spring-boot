852c875943812e2f70a13768bb9c02880a117f1b
pengfulin <1162855925@qq.com>
2024-01-10 20:12:16 +0800

    -api完善

--- boot-web/boot-webmvc/src/main/java/web/mvc/controller/FileController.java
+++ boot-web/boot-webmvc/src/main/java/web/mvc/controller/FileController.java
@@ -4,23 +4,22 @@ package web.mvc.controller;
 import commons.holder.ServletHolder;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.InputStream;
 
 /**
- * 多部分处理控制器
+ * 文件处理控制器
  *
  * @author pengshuaifeng
  * 2023/12/30
  */
-@Api(tags = "业务功能-多部分处理演示")
+@Api(tags = "业务功能-文件处理演示")
 @RestController
 @RequestMapping("/mvc/multipart")
-public class MultipartController {
+public class FileController {
 
     @GetMapping("/download")
     public void download(@RequestParam String classPath){
-        InputStream inputStream = MultipartController.class.getResourceAsStream(classPath);
+        InputStream inputStream = FileController.class.getResourceAsStream(classPath);
         String fileName = classPath.substring(classPath.lastIndexOf("/") + 1);
         ServletHolder.responseToOutStream(null,inputStream,0,fileName,null);
     }
