9ddc23e6f97e82dae105689de9b71d4f70e00732
pengfulin <1162855925@qq.com>
2024-01-25 11:22:56 +0800

    -完善通用查询模型

--- boot-web/boot-common/src/main/java/commons/model/web/request/query/GeneralQueryParam.java
+++ boot-web/boot-common/src/main/java/commons/model/web/request/query/GeneralQueryParam.java
@@ -1,9 +1,11 @@
 package commons.model.web.request.query;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-
+import org.springframework.format.annotation.DateTimeFormat;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -12,5 +14,16 @@ public  class GeneralQueryParam<I> {
 
     @ApiModelProperty(value = "业务id集")
     private List<I> ids;
+
+
+    @ApiModelProperty(value = "开始时间",example = "2024-01-01 00:00:00")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date startTime;
+
+    @ApiModelProperty(value = "截止时间",example = "2024-01-01 00:00:00")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date endTime;
 
 }
