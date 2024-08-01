72d4eb5d19caadb21bfd3a9492066d57bcd8bdf1
pengfulin <1162855925@qq.com>
2024-08-01 16:37:47 +0800

    -加入druid数据源模块&拆分boot-data模型数据源模块为独立内容

---
+++ boot-data/data-jdbc/jdbc-data_source-druid/src/main/java/jdbc/data/source/druid/ApplicationStart.java
@@ -0,0 +1,18 @@
+package jdbc.data.source.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author PengFuLin
+ * @version 1.0
+ * @description: 启动类
+ * @date 2022/3/7 16:43
+ */
+@SpringBootApplication
+public class ApplicationStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class,args);
+    }
+}
