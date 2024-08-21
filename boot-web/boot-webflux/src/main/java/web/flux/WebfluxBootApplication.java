ce1ef6d1dacbec1e13342284f06c5af2ab0cda1a
fulin-peng-ydxs <pengshuaifeng1897@gmail.com>
2024-08-21 22:10:17 +0800

    -redis session设置有效期

--- boot-web/boot-webflux/src/main/java/web/flux/WebfluxBootApplication.java
+++ boot-web/boot-webflux/src/main/java/web/flux/WebfluxBootApplication.java
@@ -2,10 +2,9 @@ package web.flux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 
-@EnableRedisWebSession
+@EnableRedisWebSession(maxInactiveIntervalInSeconds = 3600)
 @SpringBootApplication
 public class WebfluxBootApplication {
 
