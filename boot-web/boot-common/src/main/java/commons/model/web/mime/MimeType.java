9d4279632b4a06c034d554ee7cad41e0b44ff147
pengfulin <1162855925@qq.com>
2024-11-02 17:47:46 +0800

    -fix

--- boot-web/boot-common/src/main/java/commons/model/web/mime/MimeType.java
+++ boot-web/boot-common/src/main/java/commons/model/web/mime/MimeType.java
@@ -14,6 +14,8 @@ public class MimeType {
     //json
     public final static String APPLICATION_JSON_UTF_8 = "application/json;charset=utf-8";
     public final static String APPLICATION_JSON = "application/json";
+    //yaml
+    public final static String APPLICATION_YAML = "application/x-yaml";
     //pdf
     public final static String APPLICATION_PDF = "application/pdf";
     //二进制：它通常用于表示二进制数据文件的内容类型。没有特定的数据格式或结构，它通常用于指示数据是未知的、不可解释的二进制数据。
